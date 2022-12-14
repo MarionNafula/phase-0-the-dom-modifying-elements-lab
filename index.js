3239c2bbdef3f06a1b659c0db07b30b363fc7dfa
MarionNafula <wekesamarion100@gmail.com>
2022-12-14 14:14:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-// Write your code here!
-const main= document.getElementById('main') ;
-main.removeAttribute('id') ;
-
-const newHeader = document.createElement('h1')
-newHeader.id ='victory'
-newHeader.innerHTML ="YOUR-NAME is the champion"
-Footer
+document.querySelector('#main').remove();
+var newHeader = document.querySelector('h1#victory');
